d027b37badd512b26262985b7c660c9855952524
D'Arcy Rittich <drittich@gmail.com>
2026-07-03 13:48:49 -0400

    feat: increase width of update available dialog for better visibility

--- src/Sprocket.App/UpdateDialogs.cs
+++ src/Sprocket.App/UpdateDialogs.cs
@@ -81,7 +81,7 @@ internal static class UpdateAvailableDialog
         {
             Title = "Update Available",
             Icon = AppIcon.Window,
-            Width = 470,
+            Width = 560,
             Height = 240,
             CanResize = false,
             WindowStartupLocation = WindowStartupLocation.CenterOwner,
